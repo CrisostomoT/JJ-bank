3864e16685f3f0d51c6679dac01902c2b9f4f88e
Diego <diegolmi@hotmail.com>
2020-03-09 13:41:40 -0300

    master

---
+++ app/src/HomeBanking/CardClientes.js
@@ -0,0 +1,19 @@
+const CardCliente = (nombre, Apellido, Dni, Email,  Direccion, Ciudad, Provincia) => `
+   <tr>
+                <td>${nombre}</td>
+                <td>${Apellido}</td>
+                <td>${Dni}</td>
+                <td>${Email}</td>
+                <td>${Direccion}</td>
+                <td>${Ciudad}</td>
+                <td>${Provincia}</td>
+                <td><button type="button" class="btn btn-outline-warning">Enviar</button>
+                <button type="button" class="btn btn-outline-danger">Denegado</button></td>
+                </tr>
+       
+ `;
+
+
+
+
+export default CardCliente
