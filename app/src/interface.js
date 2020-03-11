188c739111947218bc7521718a640dce02202d5f
Alejandro Isa <alejandroisa71@gmail.com>
2020-03-11 11:37:29 -0300

    cambio de nombre de variable en local storage

--- app/src/interface.js
+++ app/src/interface.js
@@ -5,11 +5,11 @@ document.getElementById('sesion').addEventListener('submit', function () {
     event.preventDefault();
     let mail = inputEmail.value;
     let password = inputPassword.value;
-    // datos=['juan@gmail','1234'];
-    // localStorage.setItem('datosRegistrados', JSON.stringify(datos));
+    datos=['juan@gmail','1234'];
+    localStorage.setItem('datosRegistrados', JSON.stringify(datos));
 
     let datos = JSON.parse(localStorage.getItem("datosRegistrados"));
-
+    console.log('datos'+datos);
     if (datos.indexOf(mail) != -1 && datos.indexOf(password) != -1) {
         alert("Bienvenido");
         location.href = "profile.html";
