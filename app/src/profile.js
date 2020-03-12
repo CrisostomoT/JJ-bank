82c276c7311f157b00fa737fcdf7a7efd0a2a560
Gaby Flores <gabrielaflores671@gmail.com>
2020-03-12 13:07:33 -0300

    terminado el modal por completo de la parte de prestamos

--- app/src/profile.js
+++ app/src/profile.js
@@ -4,9 +4,6 @@ import '../../node_modules/startbootstrap-sb-admin-2/vendor/jquery/jquery.min.js
 import Swal from 'sweetalert2';
 
 import './index.scss';
-<<<<<<< HEAD
 import './styleUsser.scss';
 
 import './logicaPrestamos';
-=======
->>>>>>> 10929f7e8e72bb33f2cee83b163a6069a42e71ab
