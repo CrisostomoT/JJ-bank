fbd888cc614d0fe9fc41b1074c3e6744d7c2f6c0
esteban <estebanvan9@gmail.com>
2020-03-12 17:52:15 -0300

    hola

---
+++ app/src/prestamosAdmin.js
@@ -0,0 +1,16 @@
+let Prestamos =  JSON.parse(localStorage.getItem("DatosPrestamos")) || [];
+
+ export function prestamoAdmin() {
+    let monto = document.getElementById('monto');
+    let cuota = document.getElementById('cuota');
+    let resultado = document.getElementById('resultado1');
+    Prestamos.push({
+        Monto : monto,
+        Cuota : cuota,
+        cuotaMensual : resultado
+    })
+    localStorage.setItem("DatosPrestamos", JSON.stringify(Prestamos));
+    console.log(prestamosAdmin)
+}
+
+document.getElementById('btn-prestamo').addEventListener('click',prestamosAdmin());
