eba71f8a51b9bfbc33a611dae96c50a3a1902bb0
Alejandro Isa <alejandroisa71@gmail.com>
2020-03-13 10:35:42 -0300

    cambio en archivo interface y sesion

--- app/src/interface.js
+++ app/src/interface.js
@@ -1,23 +1,23 @@
 const a = require('./sesion.js')
 
-// DOM
 document.getElementById('sesion').addEventListener('submit', function () {
     event.preventDefault();
-    let mail = inputEmail.value;
+    let email = inputEmail.value;
     let password = inputPassword.value;
-    datos=['juan@gmail','1234'];
-    localStorage.setItem('datosRegistrados', JSON.stringify(datos));
-
-    let datos = JSON.parse(localStorage.getItem("datosRegistrados"));
-    console.log('datos'+datos);
-    if (datos.indexOf(mail) != -1 && datos.indexOf(password) != -1) {
-        alert("Bienvenido");
-        location.href = "profile.html";
-    } else {
+    let ban = 0;
+    const datos = JSON.parse(localStorage.getItem("datosIngresados"));
+    for (let index = 0; index < datos.length; index++) {
+        const element = datos[index];
+        if (datos[index].Email == email && datos[index].Password == password) {
+            alert("Bienvenido " + datos[index].Nombre);
+            localStorage.setItem("datoUsuaarioActual", JSON.stringify(datos[index].Email));
+            location.href = "profile.html";
+            // document.getElementById("usuarionav").innerHTML.value =datos[index].Nombre;
+            ban = 1;
+        }
+    }
+    if (ban == 0) {
         alert("Usuario o Contraseña incorrectos!");
-    };
-})
-
-
-
+    }
+});
 
