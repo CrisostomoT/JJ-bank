fc20b6c8d3974b1d289002ddd8d4e9b688749d3e
Alejandro Isa <alejandroisa71@gmail.com>
2020-03-13 21:27:33 -0300

    cambios

--- app/src/interface.js
+++ app/src/interface.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-// const a = require('./sesion.js')
-
-// // DOM
-// let datos = JSON.parse(localStorage.getItem("datosIngresados")) ªª;
-
-// document.getElementById('sesion').addEventListener('submit', function () {
-//     event.preventDefault();
-//     let mail = document.getElementById("inputEmail").value;
-//     let password = documet.getElementById("inputPassword").value;
-//     datos.push({
-//         Email: mail,
-//         Password: password,
-
-//     }) 
-   
-//     localStorage.setItem('datosIngresados', JSON.stringify(datos));
- 
-    
-    
-      
-//     if (datos.indexOf(mail) != -1 && datos.indexOf(password) != -1) {
-//         alert("Bienvenido");
-//         location.href = "profile.html";
-//     } else {
-//         alert("Usuario o Contraseña incorrectos!");
-//     };
-// })
-
-
-
-
-
-=======
 const a = require('./sesion.js')
 
 document.getElementById('sesion').addEventListener('submit', function () {
@@ -54,5 +20,4 @@ document.getElementById('sesion').addEventListener('submit', function () {
         alert("Usuario o Contraseña incorrectos!");
     }
 });
->>>>>>> registro-alejandro
 
